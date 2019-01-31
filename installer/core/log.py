bcdb098750de863215e4ebff2fb3bd1c60ccb128
Sajeer N <sanjnur@gmail.com>
2019-01-31 14:03:00 +0530

    Relatime log storage when terraform runs is implemented

--- installer/core/log.py
+++ installer/core/log.py
@@ -12,10 +12,8 @@ class SysLog:
     def __init__(self):
         self.error_log = os.path.join(Settings.LOG_DIR, 'error.log')
         self.debug_log = os.path.join(Settings.LOG_DIR, 'debug.log')
-        self.terraform_install_log = os.path.join(
-            Settings.LOG_DIR, 'terraform_install.log')
-        self.terraform_destroy_log = os.path.join(
-            Settings.LOG_DIR, 'terraform_destroy.log')
+        self.terraform_install_log = os.path.join(Settings.LOG_DIR, 'terraform_install.log')
+        self.terraform_destroy_log = os.path.join(Settings.LOG_DIR, 'terraform_destroy.log')
 
     def write_error_log(self, msg, with_trace=True):
 
@@ -61,29 +59,30 @@ class SysLog:
 
         self.write_debug_log(K.TERRAFORM_PLAN_COMPLETED)
 
-    def write_terraform_apply_log(self, response):
-        head_msg = "Terraform Apply is done"
+    def write_terraform_apply_log_header(self):
         with open(self.terraform_install_log, 'a+') as logfile:
             logfile.write("*" * 100)
-            logfile.write("*** Terraform install Log Started")
+            logfile.write("\n*** Terraform Apply Started")
+            logfile.write("\nDateTime: %s\n" % datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
             logfile.write("*" * 100)
-            logfile.write(self._write_header(head_msg))
-            logfile.write(response[1])
-        self.write_debug_log(K.TERRAFORM_APPLY_COMPLETED)
+        self.write_debug_log(K.TERRAFORM_APPLY_STARTED)
 
-    def write_terraform_destroy_log(self, response):
-        head_msg = "Terraform Destroy is done"
+    def write_terraform_destroy_log_header(self):
         with open(self.terraform_destroy_log, 'a+') as logfile:
             logfile.write("*" * 100)
-            logfile.write("*** Terraform Destroy Log Started")
+            logfile.write("\n*** Terraform Destroy  Started ***")
+            logfile.write("\nDateTime: %s\n" % datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
             logfile.write("*" * 100)
-            logfile.write(self._write_header(head_msg))
-            logfile.write(response[1])
-
-        self.write_debug_log(K.TERRAFORM_DESTROY_COMPLETED)
+        self.write_debug_log(K.TERRAFORM_DESTROY_STARTED)
 
     def _write_header(self, head_msg=None):
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         header = "\n%s\nDateTime: %s \nMessage: %s \n" % ("*" * 100, now, head_msg)
 
         return header
+
+    def get_terraform_install_log_file(self):
+        return self.terraform_install_log
+
+    def get_terraform_destroy_log_file(self):
+        return self.terraform_destroy_log
