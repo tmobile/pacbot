a599fcc0f78870a0295e16b3fa6258c83df5464d
Kanchana <kanchana.math@ust-global.com>
2019-04-29 18:42:18 +0530

    Added missed rules and test cases from previous release

--- jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/awsrules/utils/IAMUtils.java
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/awsrules/utils/IAMUtils.java
@@ -35,13 +35,19 @@ import com.amazonaws.services.identitymanagement.model.AccessKeyMetadata;
 import com.amazonaws.services.identitymanagement.model.AttachedPolicy;
 import com.amazonaws.services.identitymanagement.model.GetPolicyVersionRequest;
 import com.amazonaws.services.identitymanagement.model.GetPolicyVersionResult;
+import com.amazonaws.services.identitymanagement.model.GetRolePolicyRequest;
+import com.amazonaws.services.identitymanagement.model.GetRolePolicyResult;
 import com.amazonaws.services.identitymanagement.model.GetUserPolicyRequest;
 import com.amazonaws.services.identitymanagement.model.GetUserPolicyResult;
 import com.amazonaws.services.identitymanagement.model.ListAccessKeysRequest;
 import com.amazonaws.services.identitymanagement.model.ListAccessKeysResult;
+import com.amazonaws.services.identitymanagement.model.ListAttachedRolePoliciesRequest;
+import com.amazonaws.services.identitymanagement.model.ListAttachedRolePoliciesResult;
 import com.amazonaws.services.identitymanagement.model.ListAttachedUserPoliciesRequest;
 import com.amazonaws.services.identitymanagement.model.ListAttachedUserPoliciesResult;
 import com.amazonaws.services.identitymanagement.model.ListPolicyVersionsRequest;
+import com.amazonaws.services.identitymanagement.model.ListRolePoliciesRequest;
+import com.amazonaws.services.identitymanagement.model.ListRolePoliciesResult;
 import com.amazonaws.services.identitymanagement.model.ListUserPoliciesRequest;
 import com.amazonaws.services.identitymanagement.model.ListUserPoliciesResult;
 import com.amazonaws.services.identitymanagement.model.PolicyVersion;
@@ -232,5 +238,144 @@ public class IAMUtils {
 		return userPoliciesResult.getAttachedPolicies();
 	}
 
+	
+	/**
+	 * This method will fetch the policy .
+	 * 
+	 * @param policyArn
+	 * @param iamClient
+	 * @return Set of actions
+	 */
+	public static Set<String> getAllowedActionsByRolePolicy(AmazonIdentityManagementClient iamClient, String roleName) {
+		Set<String> actionSet = new HashSet<>();
+		actionSet.addAll(getAttachedRolePolicyActionSet(roleName, iamClient));
+		actionSet.addAll(getInlineRolePolicyActionSet(roleName, iamClient));
+		return actionSet;
+	}
+	
+	
+	/**
+	 * Gets the attached policy.
+	 *
+	 * @param roleName
+	 *            the role name
+	 * @param iamClient
+	 *            the iam client
+	 * @param actionSet
+	 *            the action set
+	 * @return the attached policy
+	 */
+	private static Set<String> getAttachedRolePolicyActionSet(String roleName,
+			AmazonIdentityManagementClient iamClient) {
+		Set<String> actionSet = new HashSet<>();
+		String docVersion = null;
+		List<AttachedPolicy> attachedPolicies = getAttachedPolicyOfIAMRole(roleName, iamClient);
+		for (AttachedPolicy attachedPolicy : attachedPolicies) {
+			List<PolicyVersion> policyVersions = iamClient
+					.listPolicyVersions(new ListPolicyVersionsRequest().withPolicyArn(attachedPolicy.getPolicyArn()))
+					.getVersions();
+
+			for (PolicyVersion policyVersion : policyVersions) {
+				if (policyVersion.getIsDefaultVersion()) {
+					try {
+						GetPolicyVersionRequest versionRequest = new GetPolicyVersionRequest();
+						versionRequest.setPolicyArn(attachedPolicy.getPolicyArn());
+						versionRequest.setVersionId(policyVersion.getVersionId());
+						GetPolicyVersionResult versionResult = iamClient.getPolicyVersion(versionRequest);
+						try {
+							docVersion = URLDecoder.decode(versionResult.getPolicyVersion().getDocument(), "UTF-8");
+						} catch (UnsupportedEncodingException e) {
+							logger.error(e.getMessage());
+							throw new InvalidInputException(e.getMessage());
+						}
+						Policy policy = Policy.fromJson(docVersion);
+						actionSet.addAll(getActionSet(policy));
+
+					} catch (Exception e) {
+						logger.error("Error in getting policy for base account in verify account", e.getMessage());
+					}
+				}
+			}
+		}
+		return actionSet;
+	}
+	
+	/**
+	 * Gets the inline role policy.
+	 *
+	 * @param roleName
+	 *            the role name
+	 * @param amazonIdentityManagement
+	 *            the amazon identity management
+	 * @param actionSet
+	 *            the action set
+	 * @return the inline role policy
+	 */
+	private static Set<String> getInlineRolePolicyActionSet(String roleName,
+			AmazonIdentityManagementClient amazonIdentityManagement) {
+		Set<String> actionSet = new HashSet<>();
+
+		List<String> inlineRolePolicyNameList = new ArrayList<>();
+		ListRolePoliciesRequest listRolePoliciesRequest = new ListRolePoliciesRequest();
+		listRolePoliciesRequest.setRoleName(roleName);
+		ListRolePoliciesResult listRolePoliciesResult = null;
+		do {
+			listRolePoliciesResult = amazonIdentityManagement.listRolePolicies(listRolePoliciesRequest);
+			inlineRolePolicyNameList.addAll(listRolePoliciesResult.getPolicyNames());
+			listRolePoliciesRequest.setMarker(listRolePoliciesResult.getMarker());
+		} while (listRolePoliciesResult.isTruncated());
+
+		for (String policyName : inlineRolePolicyNameList) {
+			Policy policy = getInlineRolePolicy(roleName, policyName, amazonIdentityManagement);
+			actionSet.addAll(getActionSet(policy));
+		}
+		return actionSet;
+	}
+	
+	/**
+	 * This method will fetch the attached policy a particular role.
+	 * 
+	 * @param roleName
+	 * @param iamClient
+	 * @return list of AttachedPolicy
+	 */
+	public static List<AttachedPolicy> getAttachedPolicyOfIAMRole(final String roleName,
+			AmazonIdentityManagementClient iamClient) throws RuleExecutionFailedExeption {
+		ListAttachedRolePoliciesRequest attachedUserPoliciesRequest = new ListAttachedRolePoliciesRequest();
+		attachedUserPoliciesRequest.setRoleName(roleName);
+		ListAttachedRolePoliciesResult rolePoliciesResult = iamClient
+				.listAttachedRolePolicies(attachedUserPoliciesRequest);
+		return rolePoliciesResult.getAttachedPolicies();
+	}
+	
+	/**
+	 * Gets the inline role policy.
+	 *
+	 * @param roleName
+	 *            the role name
+	 * @param policyName
+	 *            the policy name
+	 * @param amazonIdentityManagement
+	 *            the amazon identity management
+	 * @return the inline role policy
+	 */
+	private static Policy getInlineRolePolicy(String roleName, String policyName,
+			AmazonIdentityManagement amazonIdentityManagement) {
+		Policy policy = new Policy();
+		try {
+			GetRolePolicyRequest policyRequest = new GetRolePolicyRequest();
+			policyRequest.setRoleName(roleName);
+			policyRequest.setPolicyName(policyName);
+			GetRolePolicyResult policyResult = amazonIdentityManagement.getRolePolicy(policyRequest);
+			String policyAsString = policyResult.getPolicyDocument();
+
+			policyAsString = java.net.URLDecoder.decode(policyAsString, "UTF-8");
+			policy = Policy.fromJson(policyAsString);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+
+		return policy;
+	}
 
 }
