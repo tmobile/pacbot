370757b3e163d55618867bce35b190c66157246e
Harminder Singh <harminder@piktorlabs.com>
2019-05-02 16:37:12 -0700

    Fixed API issue and success message after successful shutdown operation.

--- webapp/src/app/pacman-features/modules/admin/system-management/system-management.component.ts
+++ webapp/src/app/pacman-features/modules/admin/system-management/system-management.component.ts
@@ -3,9 +3,9 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); You may not use
  * this file except in compliance with the License. A copy of the License is located at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * or in the "license" file accompanying this file. This file is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or
  * implied. See the License for the specific language governing permissions and
@@ -13,6 +13,7 @@
  */
 
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
 import { environment } from '../../../../../environments/environment';
 import { CommonResponseService } from '../../../../shared/services/common-response.service';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -22,10 +23,14 @@ import { LoggerService } from '../../../../shared/services/logger.service';
 @Component({
   selector: 'app-system-management',
   templateUrl: './system-management.component.html',
-  styleUrls: ['./system-management.component.css']
+  styleUrls: ['./system-management.component.css'],
+  providers: [TitleCasePipe]
 })
 export class SystemManagementComponent implements OnInit, OnDestroy {
   pageTitle = 'System Management';
+  breadcrumbArray: any = ['Admin'];
+  breadcrumbLinks: any = ['policies'];
+  breadcrumbPresent: any = 'System Management';
   isCheckedRules = false;
   isCheckedJobs = false;
   inputValue;
@@ -39,7 +44,8 @@ export class SystemManagementComponent implements OnInit, OnDestroy {
   constructor(
     private commonResponseService: CommonResponseService,
     private router: Router,
-    private logger: LoggerService
+    private logger: LoggerService,
+    private titleCasePipe: TitleCasePipe
   ) { }
 
   ngOnInit() {
@@ -57,7 +63,7 @@ export class SystemManagementComponent implements OnInit, OnDestroy {
       if (this.selectedValue === 'rule') {
           this.postOperations(this.selectedValue, this.isCheckedRules);
         }else if (this.selectedValue === 'job') {
-          this.postOperations(this.selectedValue, this.isCheckedRules);
+          this.postOperations(this.selectedValue, this.isCheckedJobs);
       }
       // this.OpenModal = false;
     }
@@ -84,7 +90,7 @@ export class SystemManagementComponent implements OnInit, OnDestroy {
     const url = 'https://internal-pacbot-43782189.us-east-1.elb.amazonaws.com/api/admin/operations';
     const method = environment.systemOperations.method;
     let operation;
-      operation = jobAction === false ? 'enable' : 'disable';
+      operation = jobAction ? 'enable' : 'disable';
       // below is right way - commented currently to prevent accidental shutdown
       // operation = jobAction === false ? 'disable' : 'enable';
     const queryParams = {
@@ -93,11 +99,11 @@ export class SystemManagementComponent implements OnInit, OnDestroy {
     };
 
     this.systemSubscription = this.commonResponseService
-      .getData(url, method, '', queryParams)
+      .getData(url, method, {}, queryParams)
       .subscribe(
         response => {
           console.log(response);
-          this.errorMsg = response.data.response.message +  '\nOperation is performed.';
+          this.errorMsg = this.titleCasePipe.transform(jobType) + 's operation is performed successfully.';
           this.errorVal = 1;
           this.modalTitle = 'Success';
           this.showLoader = false;
