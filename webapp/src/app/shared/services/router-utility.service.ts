7aec178ab1b1db7643401a9b67a9a15d109c45cf
Aditya Agarwal <adiagrwl94@gmail.com>
2019-08-07 18:22:11 +0530

    changes to the workflow

--- webapp/src/app/shared/services/router-utility.service.ts
+++ webapp/src/app/shared/services/router-utility.service.ts
@@ -3,9 +3,9 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); You may not use
  * this file except in compliance with the License. A copy of the License is located at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * or in the "license" file accompanying this file. This file is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or
  * implied. See the License for the specific language governing permissions and
@@ -148,25 +148,5 @@ export class RouterUtilityService {
         }
         return false;
     }
-    public getpageLevel(routerSnapshot: ActivatedRouteSnapshot) {
-        const children = routerSnapshot.children;
-        let pageLevel = routerSnapshot.data ? routerSnapshot.data['pageLevel'] : '';
-        if (children.length > 0) {
-            const index = children.length - 1;
-            const child = children[index];
-            pageLevel = this.getpageLevel(child);
-        }
-        return pageLevel;
-}
 
-public getpageTitle(routerSnapshot: ActivatedRouteSnapshot) {
-    const children = routerSnapshot.children;
-    let title = routerSnapshot.data ? routerSnapshot.data['pageTitle'] || routerSnapshot.data['title'] : '';
-    if (children.length > 0) {
-        const index = children.length - 1;
-        const child = children[index];
-        title = this.getpageTitle(child) || title;
-    }
-    return title;
-}
 }
