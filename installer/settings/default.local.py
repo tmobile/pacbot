ea40ddf7597e8f1c48429ae7e4c223da708c22b7
Sajeer N <sanjnur@gmail.com>
2019-09-25 10:08:07 +0530

    Added listener for vulnerability

--- installer/settings/default.local.py
+++ installer/settings/default.local.py
@@ -49,5 +49,5 @@ AWS_REGION = ""
 
 # This settings enable Vulnerability feature and servie
 ENABLE_VULNERABILITY_FEATURE = False
-QUALYS_API_URL = ""
-QUALYS_INFO = ""
+QUALYS_API_URL = ""  # Qualys API Url without trailing slash
+QUALYS_INFO = ""  #Base64 encoded user:password of qualys
