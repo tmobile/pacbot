00b211acfa3db76b0029e54219cd5c83bb213162
ritesh <riteshghosh2020@gmail.com>
2019-11-18 16:30:22 +0530

    recommendation bug fix

--- webapp/src/app/pacman-features/modules/compliance/recommendations/recommendations.component.ts
+++ webapp/src/app/pacman-features/modules/compliance/recommendations/recommendations.component.ts
@@ -359,7 +359,7 @@ export class RecommendationsComponent implements OnInit, OnChanges, OnDestroy {
       'order': 1
     };
     response.forEach((element) => {
-      element['displayName'] = this.refactorFieldsService.getDisplayNameForAKey(element['category'].toLocaleLowerCase()) || element['category'];
+      element['displayName'] = this.refactorFieldsService.getDisplayNameForAKey(element['category'].trim().toLocaleLowerCase()) || element['category'];
       element['icon'] = '../../../../../assets/icons/recommand_' + element.category.trim() + '.svg';
       element['color'] = summaryTabsColor[element.category];
       element['order'] = displayOrder[element.category];
