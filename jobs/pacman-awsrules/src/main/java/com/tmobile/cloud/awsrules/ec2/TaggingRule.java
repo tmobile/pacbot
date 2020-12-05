39406089435ca1d3aa52b3c51ad12b4df083efc7
KanchanaAradhya <shujan.kanchana@gmail.com>
2020-12-05 16:57:24 +0530

    threadsfe removed

--- jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/awsrules/ec2/TaggingRule.java
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/awsrules/ec2/TaggingRule.java
@@ -58,8 +58,6 @@ public class TaggingRule extends BaseRule {
 	 *
 	 * ruleKey : check-for-missing-mandatory-tags <br><br>
 	 *
-	 * threadsafe : if true , rule will be executed on multiple threads <br><br>
-	 *
 	 * severity : Enter the value of severity <br><br>
 	 *
 	 * ruleCategory : Enter the value of category <br><br>
